30d00871ff3c05089129200d49909e44792a2ddb
Ingemarus <ingemarus@gmail.com>
2022-04-05 15:20:44 +0300

    test commit

--- workerTree/models.py
+++ workerTree/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Workers(models.Model):
     full_name= models.CharField("name", max_length=30)
-    salary= models.IntegerField("name", max_length=30)
+    salary= models.IntegerField("salay")
     boss= models.ForeignKey("self", on_delete=models.DO_NOTHING, default=None)
 
     def __str__(self) :
